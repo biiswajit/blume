ae1534ed39075103b8a3746e52028a9d46e2948b
SulagnaKundu <sulagnakundu7@gmail.com>
2024-11-14 01:26:40 +0530

    Akari-Chatbot

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,6 +4,7 @@ import { ReactNode } from "react";
 import { Provider } from "jotai";
 import { ThemeProvider } from "@/ui/theme-provider";
 import { SessionProvider } from "next-auth/react";
+import { ContextProvider } from "./classroom/[classroomId]/ContextProvider";
 
 export default function Providers({ children }: { children: ReactNode }) {
   return (
@@ -14,7 +15,7 @@ export default function Providers({ children }: { children: ReactNode }) {
         enableSystem
         disableTransitionOnChange
       >
-        <SessionProvider>{children}</SessionProvider>
+        <SessionProvider><ContextProvider>{children}</ContextProvider></SessionProvider>
       </ThemeProvider>
     </Provider>
   );
